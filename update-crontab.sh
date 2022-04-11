b221ea129b43b8e2092483d0ee36288ddd142e1e
Marcelo Carvalho <marcelo.carvalho@cleverti.com>
2022-04-11 14:05:02 +0100

    :sparkles: Add update crontab device

---
+++ update-crontab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check openvpn status script
+(
+    crontab -l
+    echo "*/1 * * * * /home/pi/scripts/openvpn/check-openvpn-status.sh"
+) | sort - | uniq - | crontab -
+
+# Delete temp files script
+(
+    crontab -l
+    echo "* 6 * * *  /home/pi/scritps/openvpn/delete-wget-files.sh"
+) | sort - | uniq - | crontab -
+
+# Restart Pi Device
+(
+    crontab -l
+    echo "* 6 * * *  sudo shutdown -r now >/dev/null 2>&1"
+) | sort - | uniq - | crontab -
